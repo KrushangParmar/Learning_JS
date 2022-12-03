4084c95651eda4ebab012d8bb2fe66e8e0191822
Krushang Parmar <parmarkrushang7@gmail.com>
2022-12-03 14:59:42 +0530

    v0.1.7 - New Changes.

---
+++ src/component/UI/ErrorModal.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./Card";
+
+const ErrorModal = (props) => {
+    return(
+        <Card>
+         <header>
+            </header>   
+        </Card>
+    )
+}
+
+export default ErrorModal;
